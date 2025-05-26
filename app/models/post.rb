1c1d9c7bc43abdd5d45f9264f72a27b86524a71b
Alicia <alicia.magro@ouidou.fr>
2025-05-26 15:49:30 +0200

    Add relation between the models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  has_many :comments, dependant: :destroy
+  belongs_to :user
+
   validates :title, :body, presence: true
   validates :title, length: { maximum: 150 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  has_many :comments
+
   validates :username, :email, :password, presence: true
   validates :password, length: { minimum: 8 }
 end
