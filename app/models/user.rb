99d53a22e6c7881ae9c802cce51ae906e948109f
Alicia <alicia.magro@ouidou.fr>
2025-05-26 16:03:06 +0200

    Fix error in the user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :comments, dependent: :destroy
-  has_many :comments
+  has_many :posts
 
   validates :username, :email, :password, presence: true
   validates :password, length: { minimum: 8 }
